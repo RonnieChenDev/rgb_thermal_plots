ae84bfdb644633b94151580876236edf4987e9b3
Xi CHEN <22250819+RonnieChenDev@users.noreply.github.com>
2024-10-05 23:57:34 +0800

    Add exception handling; add test file; update README

---
+++ tests/testCanopy.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from canopy import *
+
+print("Print objects")
+block = Block(40, (0, 40), 'yard')
+block.add_road(5, 5)
+block.add_house(10, 5)
+block.add_trees('yard')
+
+print(block)
+for item in block.items:
+    print(item.pos)
+
+block_rgb_img = block.generate_block_image()
+block_thermal_img = block.generate_thermal_image(30)
+
+print(f'block_rgb_img:\n{block_rgb_img}')
+print(f'block_thermal_img:\n{block_thermal_img}')
